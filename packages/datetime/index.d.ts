67f7cfdfbe34d7407843b6b54c3ba5a261e0c31e
H. Kamran <hkamran@hkamran.com>
2023-05-22 18:34:12 -0700

    Update types

--- packages/datetime/index.d.ts
+++ packages/datetime/index.d.ts
@@ -3,6 +3,7 @@ export function convertLongWeekdayToShort(longWeekday: string): string | null;
 export function addDays(date: Date, days: number): number;
 export function dateRange(start: Date, end: Date): Date[];
 export function convert24HourTo12Hour(time: string): string;
+export function convertToTimezoneOffsetISOString(date: Date): string;
 export type Weekday = {
     [shortName: string]: string;
 };
