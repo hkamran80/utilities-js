798e915d0ada3c4f20b634f9707ced9bedc3b042
H. Kamran <hkamran@hkamran.com>
2023-07-01 15:20:17 -0500

    Add generate random string function

--- packages/strings/index.d.ts
+++ packages/strings/index.d.ts
@@ -1,4 +1,32 @@
+/**
+ * Format a number with commas
+ * @param {number|string} number - The number to format
+ * @returns {string} Formatted number
+ */
 export function numberWithCommas(number: number | string): string;
+/**
+ * Prefix numbers less than ten with a leading zero
+ * @param {number|string} number - The number to pad
+ * @returns {string} The padded number
+ */
 export function padNumber(number: number | string): string;
+/**
+ * Capitalize the first letter of a string
+ * @param {string} string - The string to capitalize the first letter of
+ * @returns {string} The string with the first character capitalized
+ */
 export function capitalizeFirstLetter(string: string): string;
+/**
+ * Generate a "slugified" version of a string
+ *
+ * Strips a string of non-alphanumeric characters, and replaces spaces with dashes
+ * @param {string} string - The string to "slugify"
+ * @returns {string} The "slugified" string
+ */
 export function slugify(string: string): string;
+/**
+ * Generate a random alphanumeric string of a specified length
+ * @param {number} length The length of the string to generate
+ * @returns {string} A random alphanumeric string of a specified length
+ */
+export function generateRandomString(length: number): string;
--- packages/strings/index.js
+++ packages/strings/index.js
@@ -1,3 +1,5 @@
+const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 /**
  * Format a number with commas
  * @param {number|string} number - The number to format
@@ -39,3 +41,20 @@ export const slugify = (string) =>
         .replace(/[^A-Za-z0-9\s]/gm, "")
         .replace(/ /gm, "-")
         .toLowerCase();
+
+/**
+ * Generate a random alphanumeric string of a specified length
+ * @param {number} length The length of the string to generate
+ * @returns {string} A random alphanumeric string of a specified length
+ */
+export const generateRandomString = (length) => {
+    let result = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+
+    return result;
+};
